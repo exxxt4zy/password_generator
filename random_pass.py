4e357f90a0ef9f462c36d232dbeead10d9f8e25b
exxxt4zy <exxxt4zy@gmail.com>
2022-03-03 23:33:56 +0500

    correction of remarks

--- random_pass.py
+++ random_pass.py
@@ -22,22 +22,27 @@ def random_password():
 
 def random_password_generator():
     passw = random_password()
+    if len(passw) > 15:
+        ts = 0.03
+    else:
+        ts = 0.1
 
     for char in passw:
         print(char, end="", flush=True)
-        time.sleep(0.1)
+        time.sleep(ts)
     print("\n")
     os.system("clear")
 
-    choise = input(str("\n\033[36m[!] Want to write to a file? (Y or N)? ")) 
-    if choise == "Y":
+    choise = input(str("\n\033[36m[!] Want to write to a file? (Y or N)? "))
+    
+    if choise.upper() == "Y":
         filename = input(str("\033[36m[*] Enter file name: "))
         arq = open(filename, 'w')
         arq.write(passw)
         arq.close()
         print("\033[33m[*] Ready! Your password has been recorded in ", filename)
         print("\033[33m[^_^] Your password: ", passw)        
-    else:
+    elif choise.upper() == "N":
         os.system("clear")
         print("\033[33m[^_^] Your password: ", passw)
 
