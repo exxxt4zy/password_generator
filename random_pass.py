3bb28e7c2c1ea1f34842b5d48004db4a4963a56b
exxxt4zy <exxxt4zy@gmail.com>
2022-03-03 15:44:40 +0500

    Started

---
+++ random_pass.py
@@ -0,0 +1,40 @@
+import os
+import string
+import random
+import time
+
+def random_password():
+    scale = input('\033[36m[!] Enter password size: ')
+    final = int(scale.split(' ')[0])
+
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    password_gen = ''.join(random.choice(chars) for x in range(final))
+    return password_gen 
+
+def random_password_generator():
+    passw = random_password()
+
+    for char in passw:
+        print(char, end="", flush=True)
+        time.sleep(0.1)
+    print("\n")
+    os.system("clear")
+    
+    if input(str("\n\033[36m[!] Want to write to a file? (Y or N)? ")) == 'Y' or 'y':
+        filename = input(str("\033[36m[*] Enter file name: "))
+        arq = open(filename, 'w')
+        arq.write(passw)
+        arq.close()
+        print("\033[33m[^_^] Your password: ", passw)
+        
+    else:
+        os.system("clear")
+        print("\033[33m[^_^] Your password: ", passw)
+    pass
+
+
+random_password_generator()
+
+        
+         
+
